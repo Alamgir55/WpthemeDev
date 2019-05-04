d31d2ba768620d7f6f4053949ea5e5a53ea4bd5e
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-05-04 00:49:05 -0400

    Initial commit

--- wp-content/themes/learnwp/general-template.php
+++ wp-content/themes/learnwp/general-template.php
@@ -1,31 +1,24 @@
+<?php 
+/*
+Template Name: General Template
+*/
+?>
 <?php  get_header(); ?>
 <img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->hieght; ?>" width="<?php echo get_custom_header()->width; ?>" alt="">
+
 <div class="content-area">
 <main>
-<section class="slide">
-	<div class="container">
-		<div class="row">Slide</div>
-	</div>
-</section>
-<section class="services">
-	<div class="container">
-		<div class="row">Services</div>
-	</div>
-</section>
+
 <section class="middle-area">
 	<div class="container">
-		<div class="row">
-			<aside class="sidebar col-md-3">Sidebar</aside>
-			<div class="news col-md-9">
+
+			<div class="general-template">
 				<?php 
 					if(have_posts()):
 						while( have_posts()): the_post();
 				?>
 				<article>
 					<h2><?php the_title(); ?></h2>
-					<p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-					<p>Categories: <?php the_category( ' ' ); ?></p>
-					<p><?php the_tags('Tags: ', ', '); ?></p>
 					<p><?php the_content(); ?></p>
 				</article>
 				<?php 
@@ -35,15 +28,11 @@
 				<p>There's nothing yet to be displayed!</p>
 			<?php endif; ?>
 			</div>
-		</div>
-	</div>
-</section>
-<section class="map">
-	<div class="container">
-		<div class="row">Map</div>
+		
 	</div>
 </section>
 </main>
 </div>
 <?php get_footer(); ?>
+
 
