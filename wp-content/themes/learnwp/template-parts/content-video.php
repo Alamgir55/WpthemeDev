987811003e344725673e61be74718e5b48842938
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-05-06 01:06:07 -0400

    initial commit

--- wp-content/themes/learnwp/template-parts/content-video.php
+++ wp-content/themes/learnwp/template-parts/content-video.php
@@ -1,5 +1,5 @@
 <article <?php post_class(); ?>>
-	<h2><?php the_title(); ?></h2>
+	<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 	<?php the_post_thumbnail( array( 275, 275 ) ); ?>
 	<div class="meta-info">
 		<p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
